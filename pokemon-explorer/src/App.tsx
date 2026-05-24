e479f9e42d41bcc2305365dbd278b393a13cb478
wojtek pasieka <wojtek.pasiu@gmail.com>
2026-05-24 14:18:17 +0200

    init pokemon explorer app

---
+++ pokemon-explorer/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-[#111111] px-6 py-8 text-white">
+      <h1 className="text-center text-4xl font-bold text-orange-500">
+        Pokémon Explorer
+      </h1>
+    </main>
+  );
+}
+
+export default App;
